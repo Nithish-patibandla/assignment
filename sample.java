7dc0dea61c8ad333e564c0a57be3f64b4625fe7f
Nithish-patibandla <patibandlanithish16@gmail.com>
2024-01-30 13:43:00 -0600

    Edited

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         for(i=0;i<=10;i++)
 {
-        System.out.println("Hello Gitbash!");
+        System.out.println("Hello Nithish!");
     }
 }
